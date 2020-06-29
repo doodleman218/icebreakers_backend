7dd04dd9e27456b557c69cf2e6754ffa7e484dd3
doodleman218 <dcwolff218@gmail.com>
2020-06-29 17:22:20 -0500

    after raza talk work

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,7 +17,7 @@ class RoomsController < ApplicationController
   
       payload = {room_id: room.id}
       token = JWT.encode(payload, "hmac_secret", 'HS256') 
-      render json: { room: room, jwt: token }, status: :created
+      render json: { room: room, jwt: token, user: user }, status: :created
         
     else
       render json: { errors: user.errors.messages }, status: :not_acceptable
